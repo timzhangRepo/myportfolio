f8e19c73706dfa925c1ceecd13474d1fc1b2e106
timzhangRepo <tim-zhang@live.com>
2021-05-20 21:47:22 -0400

    Finished design for set design

--- src/Gallery/Gallery.js
+++ src/Gallery/Gallery.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react';
 import Grid from '@material-ui/core/Grid/Grid';
-import {BRAIN_IN_A_VAT, WU_SONG_FIGHTS_THE_TIGER, SUDANESE_RUINS, ELON_MUSK, THE_APARTMENT, JAZZ_WITH_RYAN_GOSLING_LATE_NIGHT_SHOW, INCA_KEY, CONCEPT, GRAPHICS} from "../Assets/ImageAssets";
+import {
+    BRAIN_IN_A_VAT,
+    WU_SONG_FIGHTS_THE_TIGER,
+    SUDANESE_RUINS,
+    ELON_MUSK,
+    THE_APARTMENT,
+    JAZZ_WITH_RYAN_GOSLING_LATE_NIGHT_SHOW,
+    INCA_KEY,
+    CONCEPT,
+    GRAPHICS
+} from "../Assets/ImageAssets";
 import Footer from '../Footer/Footer'
 import styles from "../Home/index.module.css";
 import {STORIES} from '../Assets/TextAssets'
+import LinearProgress from '@material-ui/core/LinearProgress';
+import Skeleton from '@material-ui/lab/Skeleton';
+
 
 class Gallery extends Component {
     constructor(props) {
@@ -11,29 +24,38 @@ class Gallery extends Component {
         this.state = {
             ItemID: props.location.pathname,
             windowWidth: window.innerWidth,
+            loading: true,
         }
     }
-    componentDidUpdate (prevProps, prevState) {
+
+    componentDidUpdate(prevProps, prevState) {
         if (prevProps.location.pathname !== this.props.location.pathname) {
             this.setState({ItemID: this.props.location.pathname});
         }
     }
     handleResize = (e) => {
-        this.setState({windowWidth:window.innerWidth})
+        this.setState({windowWidth: window.innerWidth})
     }
+
     componentDidMount() {
         window.addEventListener("resize", this.handleResize);
+        setTimeout(() => {
+            this.setState({loading: false});
+        }, 500)
     }
+
     componentWillUnmount() {
         window.addEventListener("resize", this.handleResize);
     }
-    getImageDisplaySize(){
+
+    getImageDisplaySize() {
         const {windowWidth} = this.state;
-        if(windowWidth>1200) return 1200;
-        else if(windowWidth<300) return 300;
-        else return (windowWidth-200);
+        if (windowWidth > 1200) return 1200;
+        else if (windowWidth < 300) return 300;
+        else return (windowWidth - 100);
     }
-    findGallery=(ItemID)=>{
+
+    findGallery = (ItemID) => {
         const MapGallery = {
             '/graphic-design': GRAPHICS,
             '/concept-art': CONCEPT,
@@ -47,60 +69,65 @@ class Gallery extends Component {
         }
         return MapGallery[ItemID]
     }
+
     render() {
         const {ItemID} = this.state;
+        const {loading} = this.state;
         const ImageItems = this.findGallery(ItemID);
         const Text = STORIES[ItemID]
         /*default*/
         let InlineStyles = {
-            Grid_1:{padding:'1rem 2rem 1rem 2rem'},
-            Grid_2:{padding:'1rem 2rem 1rem 2rem'},
+            Grid_1: {padding: '1rem 2rem 1rem 2rem'},
+            Grid_2: {padding: '1rem 2rem 1rem 2rem'},
         }
-        if(ItemID==='/graphic-design' || ItemID==='/concept-art'){
+        if (ItemID === '/graphic-design' || ItemID === '/concept-art') {
             /*For Non-Set Design Pages*/
             InlineStyles = {
-                Grid_1:{padding:'0'},
-                Grid_2:{padding:'0rem 2rem 1rem 2rem'},
+                Grid_1: {padding: '0'},
+                Grid_2: {padding: '0rem 2rem 1rem 2rem'},
             }
         }
         return (
             <div>
-               <Grid
-               container
-               direction="column"
-               justify="center"
-               alignItems="center"
-               >
-                   <Grid item={true}>
-                       <h3>{Text.title}</h3>
-                   </Grid>
-                   <Grid item={true} style={InlineStyles.Grid_1}>
-                       <p className={styles.p}>{Text.src}</p>
-                   </Grid>
-                   <Grid item={true} style={InlineStyles.Grid_2}>
-                       <p className={styles.p}>{Text.extra}</p>
-                   </Grid>
-                   {
-                       ImageItems.map((item)=>(
-                           <Grid item={true} key={item.id}>
-                               <img src={item.src} style={{width:this.getImageDisplaySize()}}/>
-                           </Grid>
-                       ))
-                   }
-                   <Grid item={true} xs={12} style={{'margin':'2rem 0 1rem 0'}}>
-                       <p className={styles.p}>MFA in Production Design from Chapman University, pursuing a career
-                           in Film and TV Art Department, currently based in Los Angeles.</p>
-                   </Grid>
-                   <Grid item={true}>
-                       <Footer/>
-                   </Grid>
-               </Grid>
+                {loading && <LinearProgress color={'secondary'}/>}
+                <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                >
+                    <Grid item={true}>
+                        <h3>{Text.title}</h3>
+                    </Grid>
+                    <Grid item={true} style={InlineStyles.Grid_1}>
+                        <p className={styles.p}>{Text.src}</p>
+                    </Grid>
+                    <Grid item={true} style={InlineStyles.Grid_2}>
+                        <p className={styles.p}>{Text.extra}</p>
+                    </Grid>
+                    {
+                        ImageItems.map((item) => (
+                            <Grid item={true} key={item.id}>
+                                {loading ? (<Skeleton style={{margin:'1rem'}} animation="wave" variant="rect" height={200} width={this.getImageDisplaySize()}/>) : (
+                                    <img src={item.src} style={{width: this.getImageDisplaySize()}}/>)}
+                            </Grid>
+                        ))
+                    }
+                    <Grid item={true} xs={12} style={{'margin': '2rem 0 1rem 0'}}>
+                        <p className={styles.p}>MFA in Production Design from Chapman University, pursuing a career
+                            in Film and TV Art Department, currently based in Los Angeles.</p>
+                    </Grid>
+                    <Grid item={true}>
+                        <Footer/>
+                    </Grid>
+                </Grid>
 
             </div>
         );
     }
 }
+
 Gallery.defaultProps = {
-    itemID:'',
+    itemID: '',
 }
 export default Gallery;
--- src/Theme/ThemeDark.js
+++ src/Theme/ThemeDark.js
@@ -1,4 +1,5 @@
 import {createMuiTheme} from "@material-ui/core";
+import purple from '@material-ui/core/colors/purple';
 export const themeDark = createMuiTheme({
     palette:{
         type:"dark",
